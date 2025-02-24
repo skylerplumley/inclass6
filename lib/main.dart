9107699b8c98095eae16dffbd6cb1d3c67b29712
Skyler Plumley <skylerplumley@gmail.com>
2025-02-24 13:50:56 -0500

    Base code created with floating point buttons and increment/decrement functions

--- lib/main.dart
+++ lib/main.dart
@@ -47,10 +47,15 @@ void setupWindow() {
 /// [ChangeNotifier] is a class in `flutter:foundation`. [Counter] does
 /// _not_ depend on Provider.
 class Counter with ChangeNotifier {
-  int value = 0;
+  int age = 0;
 
   void increment() {
-    value += 1;
+    age += 1;
+    notifyListeners();
+  }
+
+  void decrement() {
+    age -= 1;
     notifyListeners();
   }
 }
@@ -78,48 +83,38 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Demo Home Page'),
+        title: const Text('Age Counter'),
+        backgroundColor: Colors.blue,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
-// Consumer looks for an ancestor Provider widget
-// and retrieves its model (Counter, in this case).
-// Then it uses that model to build widgets, and will trigger
-// rebuilds if the model is updated.
             Consumer<Counter>(
               builder: (context, counter, child) => Text(
-                '${counter.value}',
+                'I am ${counter.age} years old',
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
             ),
+            const SizedBox(height: 30),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton.large(
+                  onPressed: () => context.read<Counter>().increment(),
+                  tooltip: 'Increase Age',
+                  child: const Text('Increase Age'),
+                ),
+                const SizedBox(height: 30),
+                FloatingActionButton.large(
+                  onPressed: () => context.read<Counter>().decrement(),
+                  tooltip: 'Decrease Age',
+                  child: const Text('Decrease Age'),
+                ),
+              ],
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-// You can access your providers anywhere you have access
-// to the context. One way is to use Provider.of<Counter>(context).
-// The provider package also defines extension methods on the context
-// itself. You can call context.watch<Counter>() in a build method
-// of any widget to access the current state of Counter, and to ask
-// Flutter to rebuild your widget anytime Counter changes.
-//
-// You can't use context.watch() outside build methods, because that
-// often leads to subtle bugs. Instead, you should use
-// context.read<Counter>(), which gets the current state
-// but doesn't ask Flutter for future rebuilds.
-//
-// Since we're in a callback that will be called whenever the user
-// taps the FloatingActionButton, we are not in the build method here.
-// We should use context.read().
-          var counter = context.read<Counter>();
-          counter.increment();
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
